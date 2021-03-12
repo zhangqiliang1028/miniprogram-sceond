2434bb2e4b247a23817625d98639691f73123025
3027130006 <3027130006@qq.com>
2021-03-12 21:08:48 +0800

    添加了地图map

--- miniprogram/pages/AR/pages/map/map.js
+++ miniprogram/pages/AR/pages/map/map.js
@@ -1,20 +1,47 @@
-// pages/AR/pages/main/main.js
+//const { init } = require("wx-server-sdk")
+
+// pages/AR/pages/map/map.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    screenHeight: wx.getSystemInfoSync().windowHeight,
+    screenWidth: wx.getSystemInfoSync().windowWidth,
+    longitude:0,
+    latitude:0,
+    speed:0,
+    accuracy:0,
+    controls:{},
+    markers:{},
+    polyline:{},
+    circles :{},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.init()
 
   },
-
+  init:function(){
+    var that = this //函数内部的this不是整体this
+    wx.getLocation({
+      type: 'wgs84',
+      success (res) {
+        that.setData({
+          latitude:res.latitude,
+          longitude:res.longitude,
+          speed:res.speed,
+          accuracy:res.accuracy,
+        })
+      }
+     })
+     
+     
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -62,15 +89,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-  threeDClick:function(){
-    wx.navigateTo({
-      url: '../AR/pages/threeD/three'
-})
-  },
-  mapClick:function(){
-    wx.navigateTo({
-      url: '../AR/pages/map/map'
-})
-  },
+  }
 })
