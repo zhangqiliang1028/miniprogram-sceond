ce14f1a5cb1c8e7cf38a597ae810cc9fbab04e53
3027130006 <3027130006@qq.com>
2021-03-13 20:03:30 +0800

    添加了地图路线连线

--- miniprogram/pages/AR/pages/map/map.js
+++ miniprogram/pages/AR/pages/map/map.js
@@ -9,16 +9,42 @@ Page({
   data: {
     screenHeight: wx.getSystemInfoSync().windowHeight,
     screenWidth: wx.getSystemInfoSync().windowWidth,
-    longitude:0,
-    latitude:0,
+    longitude:104.0680165911,
+    latitude:30.5491861989,
     speed:0,
+    scale:5,
     accuracy:0,
-    controls:{},
-    markers:{},
-    polyline:{},
-    circles :{},
+    controls:[{
+      id:1,iconPath:'./images/1.jpg',
+      position:{left:0,top:250,width:30,height:30},
+      clickable:true
+    }],
+    markers:[
+      {iconPath:"./images/1.jpg",id:0,latitude:30.5491861989,longitude:104.0680165911,width:30,height:30,alpha:0.3,title:'wowo'},
+      {iconPath:"./images/1.jpg",id:1,latitude:30.5468832218,longitude:104.0568588833,width:30,height:30,alpha:0.3,title:'wowo'},
+    ],
+    polyline:[{
+      points:[
+        {longitude:104.0680165911,latitude:30.5491861989},
+        {longitude:104.0687752749,latitude:30.5493465980},
+        {longitude:104.0688698344,latitude:30.5470634483},
+        {longitude:104.0568588833,latitude:30.5468832218},
+      ],
+      color:"#0000ffDD",
+      width:2,
+      dottedLine:true
+    }],
+    circles :[],
   },
-
+  regionchange(e){
+    console.log(e.type);
+  },
+  markertap(e){
+    console.log(e.type);
+  },
+  controltap(e){
+    console.log(e.type);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -36,6 +62,19 @@ Page({
           longitude:res.longitude,
           speed:res.speed,
           accuracy:res.accuracy,
+          markers:[
+            {iconPath:"./images/1.jpg",id:0,latitude:res.latitude,longitude:res.longitude,width:30,height:30,alpha:0.3,title:'wowo'},
+            {iconPath:"./images/1.jpg",id:1,latitude:30.5468832218,longitude:104.0568588833,width:30,height:30,alpha:0.3,title:'wowo'},
+          ],
+          polyline:[{
+            points:[
+              {longitude:104.0680165911,latitude:30.5491861989},
+              {longitude:res.longitude,latitude:res.latitude},
+            ],
+            color:"#0000ffDD",
+            width:2,
+            dottedLine:true
+          }],
         })
       }
      })
